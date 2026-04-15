2051d4ad419fc3d5ef74067ad928d3fbc96e23b2
AK <kingdodgemangay@gmail.com>
2026-04-15 10:55:08 -0500

    added x

--- ContentView.swift
+++ ContentView.swift
@@ -5,69 +5,60 @@ struct ContentView: View {
     @State var number2 = 0
     @State var answer = 0
     var body: some View {
-        Text("Enter number 1 here")
-            .foregroundStyle(.green)
-        TextField("", value: $number1, format: .number)
-            .font(.title2)
-            .textFieldStyle(.roundedBorder)
-            .foregroundStyle(.blue)
-        Text("Enter number 2 here")
-            .foregroundStyle(.green)
-        TextField("", value: $number2, format: .number)
-            .font(.title2)
-            .foregroundStyle(.blue)
-            .textFieldStyle(.roundedBorder)
-        
-        Button("Add") {
-            answer = number1 + number2
-        }
-        .font(.largeTitle)
-        .frame(width: 200, height: 70)
-        .background(Color.pink)
-        .foregroundStyle(.white)
-        
-        Button("Subtract") {
-            answer = number1 - number2
-        }
-        .font(.largeTitle)
-        .frame(width: 200, height: 70)
-        .background(Color.pink)
-        .foregroundStyle(.white)
-        Button("Multiply") {
-            answer = number1 * number2
-        }
-        .font(.largeTitle)
-        .frame(width: 200, height: 70)
-        .background(Color.pink)
-        .foregroundStyle(.white)
-            Button("Subtract") {
-                answer = number1 - number2
-            }
-            .font(.largeTitle)
-            .frame(width: 200, height: 70)
-            .background(Color.pink)
-            .foregroundStyle(.white)
-            Button("Multiply") {
-                answer = number1 * number2
-            }
-            .font(.largeTitle)
-            .frame(width: 200, height: 70)
-            .background(Color.pink)
-            .foregroundStyle(.white)
-        Button("Divide") {
-            answer = number1 / number2
-        }
-        .font(.largeTitle)
-        .frame(width: 200, height: 70)
-        .background(Color.pink)
-        .foregroundStyle(.white)
-        Text("\(answer)")
-    
-            
-            Text("\(answer)")
-            .foregroundStyle(.red)
-            .font(.largeTitle)
-            
-        }
+   
+                
+                
+                Text("Enter number 1 here")
+                    .foregroundStyle(.green)
+                TextField("", value: $number1, format: .number)
+                    .font(.title2)
+                    .textFieldStyle(.roundedBorder)
+                    .foregroundStyle(.blue)
+                Text("Enter number 2 here")
+                    .foregroundStyle(.green)
+                TextField("", value: $number2, format: .number)
+                    .font(.title2)
+                    .foregroundStyle(.blue)
+                    .textFieldStyle(.roundedBorder)
+                
+                Button("Add") {
+                    answer = number1 + number2
+                }
+                .font(.largeTitle)
+                .frame(width: 200, height: 70)
+                .background(Color.pink)
+                .foregroundStyle(.white)
+                
+                Button("Subtract") {
+                    answer = number1 - number2
+                }
+                .font(.largeTitle)
+                .frame(width: 200, height: 70)
+                .background(Color.pink)
+                .foregroundStyle(.white)
+                Button("Multiply") {
+                    answer = number1 * number2
+                }
+                .font(.largeTitle)
+                .frame(width: 200, height: 70)
+                .background(Color.pink)
+                .foregroundStyle(.white)
+                
+                
+                Button("Divide") {
+                    answer = number1 / number2
+                }
+                .font(.largeTitle)
+                .frame(width: 200, height: 70)
+                .background(Color.pink)
+                .foregroundStyle(.white)
+                
+                
+                
+                Text("\(answer)")
+                    .foregroundStyle(.red)
+                    .font(.largeTitle)
+         
     }
-    
+}
+
