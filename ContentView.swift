ab77e1525fc30c54c2fbfc13c8bddeeb680f9028
Olivia <olivia.szeszko@gmail.com>
2026-04-15 10:33:15 -0500

    added a textfield bound to number 2

--- ContentView.swift
+++ ContentView.swift
@@ -2,9 +2,9 @@ import SwiftUI
 
 struct ContentView: View {
     @State var number1 = 0
-    
     @State var number2 = 0
     var body: some View {
-        
+        TextField("", value: $number2, format: .number)
+            
     }
 }
