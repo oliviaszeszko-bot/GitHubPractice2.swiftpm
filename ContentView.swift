0986f1814284e3222fa18f0e6b0f34857a6ede86
AK <kingdodgemangay@gmail.com>
2026-04-15 10:49:28 -0500

    jhzsd

--- ContentView.swift
+++ ContentView.swift
@@ -6,12 +6,15 @@ struct ContentView: View {
     @State var answer = 0
     var body: some View {
         Text("Enter number 1 here")
+            .foregroundStyle(.green)
         TextField("", value: $number1, format: .number)
-        
+            .font(.title2)
             .textFieldStyle(.roundedBorder)
             .foregroundStyle(.blue)
         Text("Enter number 2 here")
+            .foregroundStyle(.green)
         TextField("", value: $number2, format: .number)
+            .font(.title2)
             .foregroundStyle(.blue)
             .textFieldStyle(.roundedBorder)
             
@@ -22,10 +25,14 @@ struct ContentView: View {
         .frame(width: 200, height: 70)
         .background(Color.pink)
         .foregroundStyle(.white)
-        Button("subtract") {
+        
         Button("Subtract") {
             answer = number1 - number2
         }
+        .font(.largeTitle)
+        .frame(width: 200, height: 70)
+        .background(Color.pink)
+        .foregroundStyle(.white)
         Button("Multiply") {
             answer = number1 * number2
         }
@@ -33,15 +40,12 @@ struct ContentView: View {
         .frame(width: 200, height: 70)
         .background(Color.pink)
         .foregroundStyle(.white)
-            Button("Multiply") {
-                answer = number1 * number2
-            }
-            .font(.largeTitle)
-            .frame(width: 200, height: 70)
-            .background(Color.pink)
-            .foregroundStyle(.white)
+    
             
             Text("\(answer)")
+            .foregroundStyle(.red)
+            .font(.largeTitle)
+            
         }
     }
 
