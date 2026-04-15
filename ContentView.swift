5986f3b0ddbe30f57fb065988f7823a59c888dc6
AK <kingdodgemangay@gmail.com>
2026-04-15 10:45:40 -0500

    qol

--- ContentView.swift
+++ ContentView.swift
@@ -5,14 +5,21 @@ struct ContentView: View {
     @State var number2 = 0
     @State var answer = 0
     var body: some View {
+        Text("Enter number 1 here")
         TextField("", value: $number1, format: .number)
-    
+            .textFieldStyle(.roundedBorder)
+            .foregroundStyle(.blue)
+        Text("Enter number 2 here")
         TextField("", value: $number2, format: .number)
+            .foregroundStyle(.blue)
+            .textFieldStyle(.roundedBorder)
+            
         Button("Add") {
             answer = number1 + number2
         }
-        Button("subtract") {
+        Button("Subtract") {
             answer = number1 - number2
+        }
         Button("Multiply") {
             answer = number1 * number2
         }
