47eceb2a1205eea108f7e87ff0e067cd4869a6ab
AK <kingdodgemangay@gmail.com>
2026-04-15 10:35:44 -0500

    hjdr

--- ContentView.swift
+++ ContentView.swift
@@ -5,8 +5,10 @@ struct ContentView: View {
     @State var number2 = 0
     var body: some View {
         TextField("", value: $number1, format: .number)
-        
         TextField("", value: $number2, format: .number)
+        Button("Add") {
+            number1 + number2 = answer
+        }
             
     }
 }
